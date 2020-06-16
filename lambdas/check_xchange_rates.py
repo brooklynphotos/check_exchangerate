825d574dbef7020b96422422fd1145d181ba90de
Guoneng Zhong <guoneng.list@gmail.com>
2020-06-16 15:59:06 +0200

    Version that works on AWS lambda

---
+++ lambdas/check_xchange_rates.py
@@ -0,0 +1,54 @@
+import urllib.request, json 
+import boto3
+import datetime
+from decimal import Decimal
+
+exchange_url = 'http://localhost:8000/response.json'
+# exchange_url = 'https://openexchangerates.org/api/latest.json?app_id=eb33250b17004cce8de61e6d6e331e6c&symbols=CHF'
+
+session = boto3.Session(profile_name="dbadmin")
+appName = "exchange_rate"
+dynamodb = session.resource('dynamodb')
+def get_rate():
+  with urllib.request.urlopen(exchange_url) as url:
+    data = json.loads(url.read().decode())
+    return data['rates']['CHF']
+
+def write_rate(rate):
+  previousId = get_last_id()
+  table = dynamodb.Table('exchange_rates')
+  this_id = previousId + 1
+  timestamp = int(datetime.datetime.now().timestamp())
+  table.put_item(Item={
+    "id": this_id,
+    "rate": Decimal(str(rate)),
+    "timestamp": timestamp,
+    "previousId": previousId
+  })
+  write_last_id(this_id)
+  return this_id, rate
+
+def write_last_id(new_id):
+  table = dynamodb.Table('constants')
+  return table.update_item(
+    Key={
+      'appName': appName
+    },
+    UpdateExpression='set appValue=:v',
+    ExpressionAttributeValues={
+      ':v': new_id
+    }
+  )
+
+def get_last_id():
+  table = dynamodb.Table('constants')
+  response = table.get_item(Key={"appName":appName})
+  return response["Item"]["appValue"] if "Item" in response else 0
+
+def main():
+  # print(get_last_id().value)
+  id, r = write_rate(get_rate())
+  print("id:", id, "rate:", r)
+
+if __name__=='__main__':
+  main()
