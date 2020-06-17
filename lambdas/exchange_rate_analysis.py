8ee3079256494b8586da00d5147afa4ce9c0ec2d
Guoneng Zhong <guoneng.list@gmail.com>
2020-06-17 08:41:09 +0200

    payload bug

--- lambdas/exchange_rate_analysis.py
+++ lambdas/exchange_rate_analysis.py
@@ -7,13 +7,15 @@ dynamodb = session.resource('dynamodb')
 table = dynamodb.Table('exchange_rates')
 
 def lambda_handler(event, context):
+  print("event:",event)
   msg = event["Records"][0]["Sns"]["Message"]
   msg = msg.replace('\n','')
-  print(msg)
+  print("message:", msg)
   data = json.loads(msg)
-  latest_id = data["id"]
+  latest_id = data["responsePayload"]["id"]
   difference = find_difference(latest_id)
   difference = float(difference) if difference else None
+  print("difference", difference)
   return {
     'statusCode': 200,
     'difference': difference
@@ -40,6 +42,7 @@ def find_by_id(id):
     return None
   return items[0]
 
-if __name__=='__main__':
-  event = {'Records': [{'EventSource': 'aws:sns', 'EventVersion': '1.0', 'EventSubscriptionArn': 'arn:aws:sns:eu-west-1:575798484766:checkExchangeRate:7d4b3c50-4e32-431d-b2ab-527b3bfb69df', 'Sns': {'Type': 'Notification', 'MessageId': '7c429aa0-6cad-535a-bcfb-cdc3491c2fe6', 'TopicArn': 'arn:aws:sns:eu-west-1:575798484766:checkExchangeRate', 'Subject': None, 'Message': '{\n        "statusCode": 200,\n        "rate": 0.948138,\n        "id": 10\n    }', 'Timestamp': '2020-06-16T19:54:25.330Z', 'SignatureVersion': '1', 'Signature': 'fBPc5e+pzMgGIQZqNk5XTHw1etdmy6NC1fO17YL4/c/Goab84UQdxWDPSdPUGFOfjZVku02hzbJDfcesqJLxgYOVJJRu78MhkZDzPrXYCYeNlLwi5KaFybaXavwAJpn8kKwGCHHkF/XTe4pFvjeixDw2wASx5y2MWIHc8mPmlnSy+w32DjuIQdZkX12dbOTanAMMluxNrK4qsK39VXxY3pvdtHBnX2zKgwQYZ52AxpbjrKROBI+hgC6+5SrfxOkwixrj28sVEBfLC9xXvT8hI9QD1QriytS+OvP5B4eUSlV7aNhX1Q7qX4+CHpqop4MO7JMgNAHIoAQtD93n6ZuxrQ==', 'SigningCertUrl': 'https://sns.eu-west-1.amazonaws.com/SimpleNotificationService-a86cb10b4e1f29c941702d737128f7b6.pem', 'UnsubscribeUrl': 'https://sns.eu-west-1.amazonaws.com/?Action=Unsubscribe&SubscriptionArn=arn:aws:sns:eu-west-1:575798484766:checkExchangeRate:7d4b3c50-4e32-431d-b2ab-527b3bfb69df', 'MessageAttributes': {}}}]}
-  print(lambda_handler(event, None))
+
+if __name__ == '__main__':
+    event = {'Records': [{'EventSource': 'aws:sns', 'EventVersion': '1.0', 'EventSubscriptionArn': 'arn:aws:sns:eu-west-1:575798484766:checkExchangeRate:7d4b3c50-4e32-431d-b2ab-527b3bfb69df', 'Sns': {'Type': 'Notification', 'MessageId': '24ba716c-411c-5c78-870f-260ff9823053', 'TopicArn': 'arn:aws:sns:eu-west-1:575798484766:checkExchangeRate', 'Subject': None, 'Message': '{"version":"1.0","timestamp":"2020-06-17T06:21:18.700Z","requestContext":{"requestId":"6ee29b0a-9d7d-405f-96cb-a190518aea23","functionArn":"arn:aws:lambda:eu-west-1:575798484766:function:checkExchangeRate:$LATEST","condition":"Success","approximateInvokeCount":1},"requestPayload":{"exchange_url": "http://gzmockendpoints.s3-website-eu-west-1.amazonaws.com/exchange_rate.json"},"responseContext":{"statusCode":200,"executedVersion":"$LATEST"},"responsePayload":{"statusCode": 200, "rate": 0.961715, "id": 31}}','Timestamp': '2020-06-17T06:21:18.717Z', 'SignatureVersion': '1', 'Signature': 'XrtUjVhmZwSUCRF7kYBQvoV8IpkWp5lcktDGFLya7dzuUb9fB01hRJkSLMHPMeNEgkkZ8Ymwj18CejfcJPSQcuipkwr4CpDQ3vgRPjbLYarP6Jc6p55zvJkRlwi8eJ/Vf1IePEAeluXAKxF/qgN0Nk6x3sFqziAuAXWbW/xcRFw8UfxhpKRQiUGA6bYv/Wz+tmw1QfL2Aw4z+3iBs+W8rFFwrybS7pRf80Mj2LwWwiUxZjKm4gM1r4wKeLUIVxSHugordTdsweSkTLunJor0hRH98auBFODR2ih+cFSdg11MvtCi76rUvMiS/nGNf5cz0YF5VwM2vw0T1DdJgirlmw==', 'SigningCertUrl': 'https://sns.eu-west-1.amazonaws.com/SimpleNotificationService-a86cb10b4e1f29c941702d737128f7b6.pem', 'UnsubscribeUrl': 'https://sns.eu-west-1.amazonaws.com/?Action=Unsubscribe&SubscriptionArn=arn:aws:sns:eu-west-1:575798484766:checkExchangeRate:7d4b3c50-4e32-431d-b2ab-527b3bfb69df', 'MessageAttributes': {}}}]}
+    print(lambda_handler(event, None))
